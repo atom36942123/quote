bcbce83ea2a5acbc35579d875d9f1c96f78c6e5d
atom36942123 <atom36942@gmail.com>
2022-08-22 13:13:27 +0530

    1st commit

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ with open("/Users/ritu/Downloads/quote.csv",mode ='r') as file:
 from script import convert
 import uuid
 for quote in quote_list:
-	img=convert(quote=quote,author=None,fg="white",image="asset/background.png",border_color="black",font_size=42,font_file="asset/times.ttf",width=1080,height=1080)
+	img=convert(quote=quote,author=None,fg="white",image="asset/background.png",border_color="black",font_size=45,font_file="asset/times.ttf",width=1080,height=1080)
 	image_name=str(uuid.uuid4())+".png"
 	image_path="/Users/ritu/Downloads/quote/"
 	img.save(image_path+image_name)
